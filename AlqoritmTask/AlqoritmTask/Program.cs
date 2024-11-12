5e905c340f3b3d800e1c8596abbeef071d059a30
MusaAbdulla <musaa-ab108@code.edu.az>
2024-11-12 23:10:45 +0400

    ddd

---
+++ AlqoritmTask/AlqoritmTask/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace AlqoritmTask
+{
+    internal class Program
+    {//Tapşırıq 5: Vurma cədvəlini hazırlamaq.
+     //static void Main(string[] args)
+     //{
+     //    int num = int.Parse(Console.ReadLine());
+     //    int[] nums = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+     //    for (int i = 0; i < nums.Length; i++)
+     //    {
+     //        Console.WriteLine(nums[i]*num);
+     //    }
+
+        //}
+        //-------------------------------------------------------------------------//
+        //Tapşırıq 4: İki tam ədəd və bir operator (+, -, *, /) daxil edin.
+        //static void Main(string[] args) 
+        //{
+        //    Console.WriteLine("Zehmet olmasa iki eded daxil edin: ");
+        //    int num1 = int.Parse(Console.ReadLine());
+        //    int num2 = int.Parse(Console.ReadLine());
+        //    Console.WriteLine(num1+num2);
+
+
+        //}
+//-----------------------------------------------------------------------------------//
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            if (num>=1000 && num <10000 )
+            {
+                
+            }
+            
+
+        }
+    }
+}
+/*
+ Tapşırıq 1: Təsadüfi təyin edilən iki ədədin hasilini hesablamaq lazımdır. 
+Hesablamanın nəticəsini istifadəçi daxil edəcək. Əgər cavab doğru olarsa ekrana bunu doğrulayan yazı çıxacaq. 
+Əks halda isə doğru cavab ekranda görünəcək.
+Tapşırıq 2: Təsadüfi təyin edilən dördrəqəmli tam ədədin (məsələn, 5678) 
+tərsini müəyyən edən proqram tərtib edin (yəni, 8765).
+Tapşırıq 3: İstifadəçı tərəfindən daxil edilən dördrəqəmli ədədin (məsələn: 6523) 
+birinci və sonuncu rəqəmlərinin yerini dəyişdirən proqram tərtib edin (nəticə: 3526).
+Tapşırıq 4: İki tam ədəd və bir operator (+, -, *, /) daxil edin.
+switch-case istifadə edərək həmin əməliyyatı həyata keçirin və nəticəni çap edin.
+Tapşırıq 5: Vurma cədvəlini hazırlamaq.
+Taoşırıq 6: İstifadəçi tərəfindən daxil edilən ədədlərin cəminin tapılması(ədədləri və ədəd sayını istifadəçi daxil edəcək )
+Tapşırıq 7: İstifadəçi tərəfindən daxil edilmiş  həqıqi ədədi tam və kəsr hissəyə ayırın
+(misal üçün 68.485 ədədini 68+0.485). Daha sonra həmin ədədin kəsr hissəsinin ilk rəqəmini (baxdığımız nümunə üçün 4 rəqəmi) ekrana çıxarın.
+Tapşırıq 8: Aylıq qaz sərfiyyatının ödənilməsi üçün proqram tərtib etməli. 
+İlkin məliumat kimi aylıq sərfiyyat daxil edilsin. Sərfiyyat ödənişinın hesablanması 3 kateqoriyaya ayrılır:
+illik sərfiyyat 1200 m3-dək (1200 m3 daxil) olan hissəsi üçün bir 12 qəpik, 1200-2500 m3 olan hissəsi üçün  20 qəpik, 2500 m3-dən çox olan hissəsi üçün isə 25 qəpik olaraq götürülsün.
+Tapşırıq 9: 2 təsadüfi ədəd yaradıb, onlardan hansının daha böyük olduğunu tap.
+ */
